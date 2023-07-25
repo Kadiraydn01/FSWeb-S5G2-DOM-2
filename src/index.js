03714a9d25f43bf56c4eaea46aa20ea0197a605b
Kadiraydn01 <kadiraydn01@gmail.com>
2023-07-25 22:43:51 +0300

    8 event eklendi

--- src/index.js
+++ src/index.js
@@ -1,9 +1,82 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
-
+document.querySelector("h1").addEventListener("click", function (e) {
+  alert("Bana tıkladın!");
+});
 
 // Kodlar buraya gelecek!
+const üstkisim = document.querySelectorAll("a");
+üstkisim[0].addEventListener("dblclick", (e) => {
+  üstkisim[0].classList.toggle("large");
+});
+
+üstkisim[1].addEventListener;
+üstkisim[2].addEventListener;
+
+üstkisim[3].addEventListener("click", () => {
+  alert("Sizinle İletişime Geçeceğiz");
+});
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+const myPics = document.getElementById("myPics");
+const context = myPics.getContext("2d");
+
+myPics.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+myPics.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+window.addEventListener("mouseup", (e) => {
+  if (isDrawing) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+function drawLine(context, x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = "black";
+  context.lineWidth = 1;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+}
+
+const form = document.getElementById("form");
+
+form.addEventListener(
+  "focus",
+  (event) => {
+    event.target.style.background = "pink";
+  },
+  true
+);
+
+form.addEventListener(
+  "blur",
+  (event) => {
+    event.target.style.background = "";
+  },
+  true
+);
+const input2 = document.getElementById("buton");
+input2.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("myBtn").click();
+  }
+});
